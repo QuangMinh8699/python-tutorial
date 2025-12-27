78b5d0731272108db1cfb3edb857f7886f1b67fb
minh <quangminh8699@gmail.com>
2025-12-27 22:27:30 +0700

    format specifiers

--- 11_format_specifiers/FormatSpecifiers.py
+++ 11_format_specifiers/FormatSpecifiers.py
@@ -9,4 +9,32 @@
 # :+ = use a plus sign to indicate positive Value
 # := = place sign to leftmost position
 # :  = insert a space before positive numbers
-# :, = comma separetor
+# :, = comma separator
+
+price1 = 3.14159
+price2 = -937.73
+price3 = 12.42
+
+print(f"price 1 is ${price1:.2f}")
+print(f"price 2 is ${price2:.1f}")
+print(f"price 3 is ${price3:.3f}")
+
+print(f"price 1 is ${price1:10}")
+print(f"price 2 is ${price2:10}")
+print(f"price 3 is ${price3:10}")
+
+print(f"price 1 is ${price1:^10}")
+print(f"price 2 is ${price2:^10}")
+print(f"price 3 is ${price3:^10}")
+
+print(f"price 1 is ${price1:+}")
+print(f"price 2 is ${price2:+}")
+print(f"price 3 is ${price3:+}")
+
+print(f"price 1 is ${price1: }")
+print(f"price 2 is ${price2: }")
+print(f"price 3 is ${price3: }")
+
+print(f"price 1 is ${price1:,}")
+print(f"price 2 is ${price2:,}")
+print(f"price 3 is ${price3:,}")
