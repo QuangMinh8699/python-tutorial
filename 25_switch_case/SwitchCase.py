ec1331c393d8673cdf7f4260fc680ceaed10e233
minh <quangminh8699@gmail.com>
2026-01-14 22:20:53 +0700

    swith case

---
+++ 25_switch_case/SwitchCase.py
@@ -0,0 +1,22 @@
+def day_of_week(day):
+    match day:
+        case 1:
+            return "Sunday"
+        case 2:
+            return "Monday"
+        case 3:
+            return "Tuesday"
+        case 4:
+            return "Wednesday"
+        case 5:
+            return "Thursday"
+        case 6:
+            return "Friday"
+        case 7:
+            return "Saturday"
+        case _:
+            return "not a valid day"
+
+print(day_of_week(1))
+        
+        
