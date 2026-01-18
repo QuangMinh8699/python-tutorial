61402b2cb3ec56a81a083bf2c7f70ae4398a356f
minh <quangminh8699@gmail.com>
2026-01-19 00:01:47 +0700

    scope resolution

--- 27_scope_resolution/ScopeResolution.py
+++ 27_scope_resolution/ScopeResolution.py
@@ -1,2 +1,33 @@
 # variable scope = where a variable is visible and accessible
 # scope resolution = (LEGB) Local -> Enclosed -> Global -> Built-in
+
+def func1():
+    a = 1
+    print(a)
+
+def func2():
+    b = 1
+    # print(a) # --> ERROR
+
+# -> a and b is local variables
+
+def func3():
+    c = 3
+    def func4():
+        print(c)
+    func4()
+
+# -> c is enclosed variable
+
+d = 4
+def func5():
+    print(d)
+
+# -> d is global variable
+
+from math import e
+
+def func6():
+    print(e)
+
+# -> e is built-in variable
