d2fedafb3bd0f23b63b3f9f1f699aa36ec5a4f2b
minh <quangminh8699@gmail.com>
2026-01-20 21:08:21 +0700

    main method

---
+++ 28_main/Main.py
@@ -0,0 +1,15 @@
+# if __name__ == __main__: (this script can be imported OR run standalone)
+#                           Functions and classes in this module can be reused
+#                           without the main block of code executing
+
+#   ex. library = Import library for functionality
+#                 When running library directly, display a help page
+
+def print_name(name):
+    print(name)
+
+def main():
+    print_name("Minh")
+
+if __name__ == "__main__":
+    main()
